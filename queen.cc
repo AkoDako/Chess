e6d4b515e5f0057333544c140937da5fec37d63c
AkoDako <63671514+AkoDako@users.noreply.github.com>
2020-04-15 16:47:16 -0400

    a
    a

---
+++ queen.cc
@@ -0,0 +1,18 @@
+#include "queen.h"
+#include "piece.h"
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+bool Queen::validMove(int col, int row) const {
+  
+}
+virtual void move(int col, int row) {
+}
+virtual int getCol() const {
+}
+virtual int getRow() const {
+}
+virtual char getColour() const {
+}
